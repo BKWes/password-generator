2b8c64647b3d5e894f070619b1d00d1a40e60841
Weston <barnesweston@gmail.com>
2022-02-19 17:57:13 -0600

    add generatePassword func; add confirm prompts for password and else if statements for user inputs

--- script.js
+++ script.js
@@ -1,8 +1,93 @@
-// Assignment code here
+// special characters
+character = ["!", "#", "$", "%", "&", "*", "-", "=", "@", "<", ">", "?", "/", "~", "+" ];
+// numeric characters
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+// alphabetical characters
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+// convert letters to uppercase
+var toUpper = function(y) {
+  return y.toUpperCase();
+};
 
+alpha2 = alpha.map(toUpper);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+var generatePassword = function() {
+  enter = parseInt(prompt("Select how long you would like your password. Choose between 8 and 128"));
+    // validates user input for too large or small selections
+    if (enter < 8 || enter > 128) {
+      enter = parseInt(alert("You must choose between 8 and 128"));
+      generatePassword();
+    } 
+    // require an entry 
+    else if (!enter) {
+      alert("Please enter a value between 8 and 128")
+    }
+    // continues if input is valid 
+    else {
+      console.log("Password length: " + enter);
+
+      confirmNumber = confirm("Will your password contain numbers?")
+      confirmCharacter = confirm("Will your password contain special characters?")
+      confirmUppercase = confirm("Will your password contain uppercase letters?")
+      confirmLowercase = confirm("Will your password contain lowercase letters?")
+    };
+// if 4 negative inputs 
+    if (!confirmNumber && !confirmCharacter && !confirmUppercase && !confirmLowercase) {
+      choices = alert("Please choose a criteria!");
+    }
+  // else if for 4 positive inputs
+    else if (confirmNumber && confirmCharacter && confirmUppercase && confirmLowercase){
+      choices = character.concat(number, alpha, alpha2);
+    }
+  // else if for 3 positive inputs
+    else if (confirmCharacter && confirmNumber && confirmUppercase) {
+      choices = character.concat(number, alpha2);
+    }
+    else if (confirmCharacter && confirmNumber && confirmLowercase) {
+      choices = character.concat(number, alpha);
+    }
+    else if (confirmCharacter && confirmLowercase && confirmUppercase) {
+      choices = character.concat(alpha, alpha2);
+    }
+    else if (confirmNumber && confirmLowercase && confirmUppercase) {
+      choices = number.concat(alpha, alpha2);
+    }
+  // else if for 2 positive inputs
+    else if (confirmNumber && confirmCharacter) {
+      choices = number.concat(character);
+    }
+    else if (confirmNumber && confirmUppercase) {
+      choices = number.concat(alpha2);
+    }
+    else if (confirmNumber && confirmLowercase) {
+      choices = number.concat(alpha);
+    }
+    else if (confirmLowercase && confirmCharacter) {
+      choices = alpha.concat(character);
+    }
+    else if (confirmUppercase && confirmCharacter) {
+      choices = alpha2.concat(character)
+    }
+    else if (confirmUppercase && confirmLowercase) {
+      choices = alpha2.concat(alpha)
+    }
+  // else if for 1 positive input
+    else if (confirmNumber) {
+      choices = number
+    }
+    else if (confirmCharacter) {
+      choices = character
+    }
+    else if (confirmUppercase) {
+      choices = alpha2
+    }
+    else if (confirmLowercase) {
+      choices = alpha
+    };
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +100,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// generatePassword();
